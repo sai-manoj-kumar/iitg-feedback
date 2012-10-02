f6a7f5b0e65854a632dd354c144c233ca3544631
Sai Manoj Kumar Yadlapati <ysaimanojkumar+github@gmail.com>
2012-10-02 22:13:26 +0530

    Identify Admin using google Users API

--- handler.py
+++ handler.py
@@ -2,18 +2,25 @@ __author__ = 'saimanoj'
 
 import os
 import webapp2
+from google.appengine.api import users
 import jinja2
 
-template_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self,template,**params):
+    def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
-    def render(self,template,**kw):
-        self.write(self.render_str(template,**kw))
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        self.user = users.get_current_user()
+
+
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 __author__ = 'saimanoj'
 
 import webapp2
+from google.appengine.api import users
 import handler
 import formA
+import download
 
 class MainPage(handler.Handler):
     def get(self):
-        self.render('index.jinja2')
+        if self.user:
+            if self.user.nickname() == "ysaimanojkumar":
+                self.redirect('/download/1a2b3cd456gh')
+            else:
+                self.render('index.jinja2')
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
-app = webapp2.WSGIApplication([ ('/', MainPage),
-                                ('/formA',formA.MainPage)],
-                                debug=True)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+    ('/formA', formA.MainPage),
+    ('/download/.*', download.MainPage)],
+    debug=True)
 
