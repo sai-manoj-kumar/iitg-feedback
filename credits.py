9219e4daed36ce12f602e585b38f97d9f227e713
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2012-11-06 23:22:18 +0530

    Update Credits Page to display link to my various Social Networking sites randomly.

--- credits.py
+++ credits.py
@@ -1,8 +1,12 @@
 __author__ = 'saimanoj'
 
 import handler
+import random
 
 class MainPage(handler.Handler):
     def get(self):
-        self.render('credits.jinja2')
+        urls = ['https://www.facebook.com/ysaimanojkumar','https://plus.google.com/116737306836663798171/posts',
+                'https://github.com/saimanoj/','https://twitter.com/ysaimanoj',
+                'http://in.linkedin.com/in/ysaimanojkumar']
+        self.render('credits.jinja2', url = random.choice(urls))
 
