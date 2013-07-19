c048b79e096cadbb6f5d4b83c6dcb930f5c742fd
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-19 20:28:45 +0530

    Change css, js to use same app's files;
    Removes twitter and linkedin from credits urls

--- credits.py
+++ credits.py
@@ -3,10 +3,10 @@ __author__ = 'saimanoj'
 import handler
 import random
 
+
 class MainPage(handler.Handler):
     def get(self):
-        urls = ['https://www.facebook.com/ysaimanojkumar','https://plus.google.com/116737306836663798171/posts',
-                'https://github.com/saimanoj/','https://twitter.com/ysaimanoj',
-                'http://in.linkedin.com/in/ysaimanojkumar']
-        self.render('credits.jinja2', url = random.choice(urls))
+        urls = ['https://www.facebook.com/ysaimanojkumar', 'https://plus.google.com/116737306836663798171/posts',
+                'https://github.com/saimanoj/']
+        self.render('credits.jinja2', url=random.choice(urls))
 
