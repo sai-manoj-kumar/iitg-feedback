dabf3f6be51c8813e8d6cf57eb2a4936a7d0eaa9
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-11-26 22:23:49 +0530

    Update urls of g+ and github in credits page

--- credits.py
+++ credits.py
@@ -6,7 +6,7 @@ import random
 
 class MainPage(handler.Handler):
     def get(self):
-        urls = ['https://www.facebook.com/ysaimanojkumar', 'https://plus.google.com/116737306836663798171/posts',
-                'https://github.com/saimanoj/']
+        urls = ['https://www.facebook.com/ysaimanojkumar', 'https://www.google.com/+SaiManojKumarYadlapati',
+                'https://github.com/ysaimanojkumar']
         self.render('credits.jinja2', url=random.choice(urls))
 
